b17cbcf230fbb59221a00d522a1c4a086dbb08e7
Malaika <mfer0073@student.monash.edu>
2025-11-01 23:02:27 +1100

    Added descriptions of what the fields mean and how to interpret the outputs

--- nitrateApp/inst/app/app.R
+++ nitrateApp/inst/app/app.R
@@ -13,30 +13,54 @@ site_map <- c(
 )
 
 # UI
-ui <- fluidPage(
+ui <- fluidPage( # opening fluidpage
   titlePanel("Exploring Nitrate Concentrations by Site (June and July 2019)"),
-  sidebarLayout(
-    sidebarPanel(
-      checkboxGroupInput(
+  sidebarLayout( # opening sidebar layout
+    sidebarPanel( # opening sidebar panel
+    checkboxGroupInput(
       inputId = "site",
       label = "Select site(s)",
       choices = setNames(names(site_map), unname(site_map)),
       selected = names(site_map)
     ),
+
     dateRangeInput(
       inputId = "daterange",
       label = "Select a date range",
       start = min(nitrates_small$date),
       end = max(nitrates_small$date),
       min = min(nitrates_small$date),
-      max = max(nitrates_small$date))
-
+      max = max(nitrates_small$date)
     ),
-    mainPanel(
+
+    tags$hr(),
+    h4("About this app"),
+    helpText(
+      "Explore daily nitrate concentrations (mg/L) for three NEON river sites from June 2019 to July 2019. ",
+      "Use the checkboxes and date range to focus on a site or period and compare patterns."
+  ),
+
+  tags$hr(),
+  h4("What these variables mean"),
+  tags$ul(
+    tags$li(tags$code("site"), " — Name of NEON site."),
+    tags$li(tags$code("date"), " — Date range between June - July 2019."),
+    tags$li(tags$code("nitrate_mgL"), " — Daily mean nitrate (mg/L).")
+  ),
+
+  tags$hr(),
+  h4("How to interpret the plot"),
+  helpText(
+    "The lines indicate the how nitrate levels at the three sites changed over time. ",
+    "Use the location checkboxes and date range to focus on a site or period and compare patterns."
+    )
+  ),
+  mainPanel(
       plotOutput("nitratePlot")
     )
   )
 )
+
 
 
 # Server
