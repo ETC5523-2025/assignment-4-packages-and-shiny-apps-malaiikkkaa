b82b6999576a7e38dee738b03bad88375ab9d9a6
Malaika <mfer0073@student.monash.edu>
2025-11-03 17:05:38 +1100

    Updated run_nitrate_app() roxygen documentation

--- R/run_nitrate_app.R
+++ R/run_nitrate_app.R
@@ -1,5 +1,17 @@
-#'Running the Shiny app
+#'Launch the nitrateApp Shiny application
 #'
-#'Opening the nitrate concentration app
+#'Opening the nitrate concentration app contained in this package to explore daily nitrate concentrations by site and date.
+#'
+#'@return Returns the result of \code{shiny::runApp()}
 #'@export
-run_nitrate_app <- function(){shiny::runApp("inst/app")}
+#'@examples
+#' if (interactive()) {
+#'   nitrateApp::run_nitrate_app()
+#' }
+run_nitrate_app <- function() {
+  app_dir <- system.file("app", package = "nitrateApp")
+  if (app_dir == "" || !dir.exists(app_dir)) {
+    stop("App directory not found inside the installed package. Try reinstalling.", call. = FALSE)
+  }
+  shiny::runApp(app_dir, display.mode = "normal")
+}
